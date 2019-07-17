aad024ae5f31d2ec5285600b595fd7ebb952bb51
duongtdn <duongtdn@gmail.com>
2019-07-17 08:48:04 +0000

    implement QuizResultSummary

--- src/quiz-player-react-plugin.js
+++ src/quiz-player-react-plugin.js
@@ -31,6 +31,7 @@ class QuizReactComponent extends Component {
     this.state = {
       data: null,
       index: 0,
+      showSummary: false
     }
 
     this.submitted = []
@@ -52,35 +53,9 @@ class QuizReactComponent extends Component {
 
   render() {
     if (this.state.data && this.state.data.length > 0) {
-      const quizzes = this.state.data
-      const quiz = quizzes[this.state.index]
       return (
         <div id = "quiz-player" className="embed-player">
-          <header className="w3-border-bottom" style={{padding: '8px 0', position: 'relative'}}>
-            <span> Quiz {this.state.index+1} </span>
-            <span className={quizzes.length === 1 ? 'w3-hide': ''} style={{position: 'absolute', top: 0, right: 0}}>
-              {
-                quizzes.map( (quiz,index) => {
-                  const color = this.submitted[index]? 'blue' : 'grey'
-                  return (
-                    <CircleTag key={index} value={index} color={color} onClick={ () => this.setState({ index })} />
-                  )
-                })
-              }
-            </span>
-          </header>
-          <div className="">
-            <Quiz data={quiz}
-                  addons={addons}
-                  updateAnswers = {this.updateAnswers}
-                  getSavedAnswers = {this.getSavedAnswers}
-                  updateInternalState = {this.props.updateInternalState}
-                  getSavedInternalState = {this.props.getSavedInternalState}
-            />
-          </div>
-          <div className="w3-border-bottom" style={{marginTop: '32px', padding: '16px 0'}}>
-            <button className="w3-button w3-blue" onClick={this.submit}> Submit </button>
-          </div>
+          {this.state.showSummary? this._renderSummary() : this._renderQuiz()}
         </div>
       )
     } else {
@@ -88,16 +63,90 @@ class QuizReactComponent extends Component {
     }
   }
 
+  _renderQuiz() {
+    const quizzes = this.state.data
+    const quiz = quizzes[this.state.index]
+    return (
+      <div >
+        <header className="w3-border-bottom" style={{padding: '8px 0', position: 'relative'}}>
+          <span> Quiz {this.state.index+1} </span>
+          <span className={quizzes.length === 1 ? 'w3-hide': ''} style={{position: 'absolute', top: 0, right: 0}}>
+            {
+              quizzes.map( (quiz,index) => {
+                const color = this.submitted[index]? 'blue' : 'grey'
+                return (
+                  <CircleTag key={index} value={index} color={color} onClick={ () => this.setState({ index, showSummary: false })} />
+                )
+              })
+            }
+          </span>
+        </header>
+        <div className="">
+          <Quiz data={quiz}
+                addons={addons}
+                updateAnswers = {this.updateAnswers}
+                getSavedAnswers = {this.getSavedAnswers}
+                updateInternalState = {this.props.updateInternalState}
+                getSavedInternalState = {this.props.getSavedInternalState}
+          />
+        </div>
+        <div className="w3-border-bottom" style={{marginTop: '32px', padding: '16px 0'}}>
+          <button className="w3-button w3-blue" onClick={this.submit}> Submit </button>
+        </div>
+      </div>
+    )
+  }
+
+  _renderSummary() {
+    const quizzes = this.state.data
+    return (
+      <div >
+        <header className="w3-border-bottom" style={{padding: '8px 0', position: 'relative'}}>
+          <span> <i className="w3-text-blue fa fa-calendar" /> Report </span>
+          <span className={quizzes.length === 1 ? 'w3-hide': ''} style={{position: 'absolute', top: 0, right: 0}}>
+            {
+              quizzes.map( (quiz,index) => {
+                const color = this.submitted[index]? 'blue' : 'grey'
+                return (
+                  <CircleTag key={index} value={index} color={color} onClick={ () => this.setState({ index, showSummary: false  })} />
+                )
+              })
+            }
+          </span>
+        </header>
+        <div className="" style={{marginTop: '32px'}}>
+          <ul className="w3-ul">
+            {
+              quizzes.map( (quiz, index) => {
+                const check = this.checks[index]
+                return (
+                  <li key = {index} style={{maxWidth: '960px', margin: 'auto'}}>
+                    <span> Quiz {index+1} </span>
+                    <span className="w3-right"> { check && Object.keys(check).every( key => check[key] ) ? <i className="w3-text-green fa fa-check" /> : <i className="w3-text-red fa fa-remove" /> } </span>
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </div>
+        <div className="w3-border-bottom" style={{marginTop: '32px', padding: '16px 0'}}>
+          <div style={{maxWidth: '960px', margin: 'auto'}}>
+            <button className="w3-button w3-blue" onClick={e => this.finish()}> Next Lesson </button>
+          </div>
+        </div>
+      </div>
+    )
+  }
+
   submit() {
     const quizzes = this.state.data
     const check = this.checkAnswer()
     if (quizzes.length > 1) {
       console.log(check?'Correct':'Incorrect')
+      this.submitted.splice(this.state.index, 1, true)
       if (this.state.index >= quizzes.length - 1) {
-        // end of quizzes
-        this.finish()
+        this.setState({ showSummary: true })
       } else {
-        this.submitted.splice(this.state.index, 1, true)
         this.setState({ index: this.state.index + 1 })
       }
     } else {
@@ -124,10 +173,10 @@ class QuizReactComponent extends Component {
     const index = this.state.index
     const quiz = quizzes[index]
     const userAnswer = this.answers[index]
-    const checks = this.checks
     const check = {}
     for (let key in quiz.answer) {
-      if (userAnswer && userAnswer[key] !== undefined && userAnswer[key] !== null && userAnswer[key] === quiz.answer[key]) {
+      // cannot compare object, need to rework
+      if (userAnswer && userAnswer[key] !== undefined && userAnswer[key] !== null && userAnswer[key] == quiz.answer[key]) {
         check[key] = true
       } else {
         check[key] = false
